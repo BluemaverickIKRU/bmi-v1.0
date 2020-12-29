f2a94e89641b54c4801dbc20f0bdc6e14e965169
Rajasekar-IKRU <73211169+BluemaverickIKRU@users.noreply.github.com>
2020-12-29 09:48:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+#Patient exersice
+"""
+Patient_Name = input('Type your name Here: ')
+patient_Age = input('Type your age Here: ')
+print("Thank you for your FeedBack :)")
+print("Name:",Patient_Name)
+print("Age:",patient_Age)
+"""
+#2nd Exercise
+"""
+Name = input("What is your Name?")
+Color = input("what is your favorite color?") 
+print(Name,"likes",Color)
+"""
+"""
+#Pound to Kilo
+weight_pound = input("Enter your weight in Pounds: ")
+weight_kilo = (int(weight_pound)*0.454)
+print("Your weight in Kilograms is:",weight_kilo)
+"""
+
+# BMI Calculator
+"""
+print("Welcome to BMI Calculator v1.0")
+print("-------------------------------")
+name = input("Type your name: ")
+age = input("Type your age: ")
+height = input("Type you height in Cm: ")
+weight = input("Type your weight in kg: ")
+height_meter = int(height) * 0.01
+bmi = int(weight) / (height_meter) **2
+print("Your BMI is:",bmi)
+print("Thank you for using our app :)")
+print("------------------------------")
+"""
+
+#If,Else clause
+"""
+a = int(input("Enter the num: "))
+
+
+if (a == 1):
+    print("Hello world")
+
+else:
+    print("Error")
+"""
