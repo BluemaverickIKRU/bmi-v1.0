1092e43bcf360281a697d291cfdc8c242a14705b
Rajasekar <73211169+BluemaverickIKRU@users.noreply.github.com>
2020-12-29 11:53:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-#Patient exersice
-"""
-Patient_Name = input('Type your name Here: ')
-patient_Age = input('Type your age Here: ')
-print("Thank you for your FeedBack :)")
-print("Name:",Patient_Name)
-print("Age:",patient_Age)
-"""
-#2nd Exercise
-"""
-Name = input("What is your Name?")
-Color = input("what is your favorite color?") 
-print(Name,"likes",Color)
-"""
-"""
-#Pound to Kilo
-weight_pound = input("Enter your weight in Pounds: ")
-weight_kilo = (int(weight_pound)*0.454)
-print("Your weight in Kilograms is:",weight_kilo)
-"""
-
-# BMI Calculator
-"""
+#BMI Calculator V0.1
 print("Welcome to BMI Calculator v1.0")
 print("-------------------------------")
 name = input("Type your name: ")
@@ -28,20 +6,17 @@ age = input("Type your age: ")
 height = input("Type you height in Cm: ")
 weight = input("Type your weight in kg: ")
 height_meter = int(height) * 0.01
-bmi = int(weight) / (height_meter) **2
+bmi = int(weight) / (height_meter) **2 
 print("Your BMI is:",bmi)
-print("Thank you for using our app :)")
-print("------------------------------")
-"""
 
-#If,Else clause
-"""
-a = int(input("Enter the num: "))
+if (bmi > 25):
+    print("Status : Your should excersise regularly :)")
 
-
-if (a == 1):
-    print("Hello world")
+elif (bmi < 18):
+    print("Status : Your should have a proper diet and eat well :)")
 
 else:
-    print("Error")
-"""
+    print("Status : Your are good.Never stop doing what you are doing now :)")    
+
+print("Thank you for using our app :)")
+print("------------------------------")
